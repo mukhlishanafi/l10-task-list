13174b39c5d1639830c7b24faee21c13c7c715d6
Mukhlis Hanafi <mukhlishanafi04@gmail.com>
2024-03-17 09:48:48 +0700

    34 - keeping old values in the form

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -3,12 +3,12 @@
 @section('title', 'Add Task')
 
 @section('styles')
-  <style>
-    .error-message {
-      color: red;
-      font-size: 0.8rem;
-    }
-  </style>
+<style>
+  .error-message {
+    color: red;
+    font-size: 0.8rem;
+  }
+</style>
 @endsection
 
 @section('content')
@@ -16,25 +16,25 @@
   @csrf
   <div>
     <label for="title">Title</label>
-    <input type="text" name="title" id="title">
+    <input type="text" name="title" id="title" value="{{old('title)}}">
     @error('title')
-      <p class="error-message">{{ $message }}</p>
+    <p class="error-message">{{ $message }}</p>
     @enderror
   </div>
 
   <div>
     <label for="description">Description</label>
-    <textarea name="description" id="description" rows="5"></textarea>
+    <textarea name="description" id="description" rows="5">{{old('description)}}</textarea>
     @error('description')
-      <p class="error-message">{{ $message }}</p>
+    <p class="error-message">{{ $message }}</p>
     @enderror
   </div>
 
   <div>
     <label for="long_description">Long Description</label>
-    <textarea name="long_description" id="long_description" rows="10"></textarea>
+    <textarea name="long_description" id="long_description" rows="10">{{old('long_description)}}</textarea>
     @error('long_description')
-      <p class="error-message">{{ $message }}</p>
+    <p class="error-message">{{ $message }}</p>
     @enderror
   </div>
 
